b972a3b2f78c7e7fdca8f001550d823f58752494
William <williamluders@hotmail.com>
2022-04-21 22:03:09 -0400

    P20 Implementation with tests.

---
+++ LeetCode/P20_ValidParentheses.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class P20_ValidParentheses
+    {
+        private static HashSet<char> openParentheses = new HashSet<char>(new char[] { '[', '{', '(' });
+        private static HashSet<char> closeParentheses = new HashSet<char>(new char[] { ']', '}', ')' });
+
+        public static bool IsValid(string s)
+        {
+            ParenthesesStack pStack = new ParenthesesStack();
+
+            foreach (char c in s)
+            {
+                if (IsOpening(c))
+                {
+                    pStack.Push(c);
+                }
+                if (IsClosing(c))
+                {
+                    if (!pStack.IsValidPop(c))
+                    {
+                        return false;
+                    }
+                    pStack.Pop();
+                }
+            }
+            return pStack.Empty;
+        }
+
+        public static bool IsOpening(char c)
+        {
+            return openParentheses.Contains(c);
+        }
+
+        public static bool IsClosing(char c)
+        {
+            return closeParentheses.Contains(c);
+        }
+
+        private class ParenthesesStack
+        {
+            Stack<char> charStack = new Stack<char>();
+
+            public void Push(char c)
+            {
+                charStack.Push(c);
+            }
+
+            public bool IsValidPop(char c)
+            {
+                return !Empty && ParenthesesMatch(charStack.Peek(), c);
+            }
+
+            public char Pop() => charStack.Pop();
+
+            public bool Empty => charStack.Count == 0;
+
+            private bool ParenthesesMatch(char openP, char closeP)
+            {
+                return (openP == '(' && closeP == ')'
+                    || openP == '{' && closeP == '}'
+                    || openP == '[' && closeP == ']');
+            }
+        }
+    }
+
+    
+
+}
---
+++ LeetCodeTests/P20_ValidParenthesesTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    public class P20_ValidParenthesesTests
+    {
+        [Test]
+        public void Parentheses_AreValid_ForEmptyString()
+        {
+            Assert.That(P20_ValidParentheses.IsValid(""), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Parentheses_AreValid_ForSimpleOpenClose()
+        {
+            Assert.That(P20_ValidParentheses.IsValid("()"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Parentheses_AreValid_ForRepeatedOpenClose()
+        {
+            Assert.That(P20_ValidParentheses.IsValid("(){}"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Parentheses_AreValid_ForNestedOpenClose()
+        {
+            Assert.That(P20_ValidParentheses.IsValid("({[]})"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Parentheses_AreValid_ForNestedAndRepeatedOpenClose()
+        {
+            Assert.That(P20_ValidParentheses.IsValid("((){[]})[]{((()))}"), Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Parentheses_AreInvalid_ForSingleOpeningParenthesis()
+        {
+            Assert.That(P20_ValidParentheses.IsValid("("), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Parentheses_AreInvalid_ForSingleClosingParenthesis()
+        {
+            Assert.That(P20_ValidParentheses.IsValid(")"), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Parentheses_AreInvalid_ForNonMatchingParentheses()
+        {
+            Assert.That(P20_ValidParentheses.IsValid("(}"), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void Parentheses_AreInvalid_ForNestedNonMatchingParentheses()
+        {
+            Assert.That(P20_ValidParentheses.IsValid("([{]})"), Is.EqualTo(false));
+        }
+    }
+}
