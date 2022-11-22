9dc30e6915b042c488884a26e29338cd88ac2256
William <williamluders@hotmail.com>
2022-11-21 21:26:03 -0500

    P206 Solution

---
+++ LeetCode/P206_ReverseLinkedList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static class P206_ReverseLinkedList
+    {
+        public static ListNode ReverseList(ListNode head, ListNode prior=null)
+        {
+            if (head == null)
+                return prior;
+            if (head.next == null && prior == null)
+                return head;
+
+            // get reference to the next
+            var next = head.next;
+            // point current to prior
+            head.next = prior;
+            // call reverse on next with prior as current
+            return ReverseList(next, head);
+        }
+    }
+}
---
+++ LeetCodeTests/P206_ReverseLinkedListTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using NUnit.Framework;
+
+namespace LeetCodeTests
+{
+    public class P206_ReverseLinkedListTests
+    {
+        [Test]
+        public void EmptyList_ReturnsEmptyList_WhenReversed()
+        {
+            ListNode node = null;
+            ListNode reversed = P206_ReverseLinkedList.ReverseList(node);
+
+            Assert.That(reversed, Is.Null);
+        }
+
+        [Test]
+        public void ListWithOneElement_ReturnsOneElement_WhenReversed()
+        {
+            ListNode node = new ListNode(0, null);
+            ListNode reversed = P206_ReverseLinkedList.ReverseList(node);
+
+            Assert.That(reversed.val, Is.EqualTo(0));
+            Assert.That(reversed.next, Is.Null);
+        }
+
+        [Test]
+        public void List_ReturnsReversedList_WhenReversed()
+        {
+            ListNode lastNode = new ListNode(0, null);
+            ListNode middleNode = new ListNode(2, lastNode);
+            ListNode firstNode = new ListNode(7, middleNode);
+
+            ListNode reversed = P206_ReverseLinkedList.ReverseList(firstNode);
+
+            Assert.That(reversed.val, Is.EqualTo(0));
+            Assert.That(reversed.next.val, Is.EqualTo(2));
+            Assert.That(reversed.next.next.val, Is.EqualTo(7));
+            Assert.That(reversed.next.next.next, Is.Null);
+        }
+    }
+}
